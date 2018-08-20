8790e7914307b3367fca2dd3b0826eb655897e08
Zeno Dierick <zeno.dierick@intelli.gent>
2018-08-20 12:31:21 +0200

    changed to netstandard20

--- OpenPop/AssemblyCommon.cs
+++ OpenPop/AssemblyCommon.cs
@@ -1,15 +1,6 @@
 ﻿using System.Reflection;
 
-// This file is linked to by other OpenPop projects to make
-// sure that commen assembly information is shared correctly
-[assembly: AssemblyVersion("2.0.6")]
-
-// This is the configuration when building the assembly
-[assembly: AssemblyConfiguration("Release mode assembly. Built for .NET 2.0")]
-
 // Just some plain boring Assembly information
-[assembly: AssemblyCompany("OpenPop")]
-[assembly: AssemblyProduct("OpenPop")]
 [assembly: AssemblyTrademark("OpenPop")]
 
 // To clarify this License:
